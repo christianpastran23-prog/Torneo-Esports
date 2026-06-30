fec6fe72213c535f64b2ae64db8fbc290b26d5c3
Christian Pastran <christianpastran23@gmail.com>
2026-06-30 16:51:31 -0400

    Forzar inyeccion estricta de datasource en usuario

--- usuario/src/main/java/com/fullstack/usuario/model/Usuario.java
+++ usuario/src/main/java/com/fullstack/usuario/model/Usuario.java
@@ -19,7 +19,7 @@ public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
     @Column(nullable = false)
     private String username;
